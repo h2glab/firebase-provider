62f4679a3834c0ea47aad1e50a5f0e28fb59a1c1
Martial Maillot <martial@h2g.io>
2018-12-18 16:25:01 +0100

    Add tests to dynamic links creation endpoint

--- Tests/FirebaseTests/Tests/DynamicLinks/DynamicLinksEndpointsTests.swift
+++ Tests/FirebaseTests/Tests/DynamicLinks/DynamicLinksEndpointsTests.swift
@@ -3,18 +3,18 @@ import Vapor
 import XCTest
 
 final class DynamicLinksEndpointsTests: XCTestCase {
-    
+
     func test_shortLinkEndpoint_value() throws {
         // Given
         let expectedEnpoint = "https://firebasedynamiclinks.googleapis.com/v1/shortLinks"
-        
+
         // When
         let endpoint = FirebaseAPIEndpoint.DynamicLinks.shortLinks.endpoint
-        
+
         // Then
         XCTAssertEqual(endpoint, expectedEnpoint)
     }
-    
+
     static var allTests = [
         ("test_shortLinkEndpoint_value", test_shortLinkEndpoint_value),
         ]
