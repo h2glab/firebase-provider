7cc5bdd5f3aedf88fd4a04a5b88caa392db2a5c5
Martial Maillot <martial@h2g.io>
2018-12-19 13:51:41 +0100

    Add warning details on dynamic links. Useful for debug

--- Sources/Firebase/Models/Responses/DynamicLink.swift
+++ Sources/Firebase/Models/Responses/DynamicLink.swift
@@ -4,5 +4,15 @@ public struct DynamicLink: FirebaseModel {
 
     public let shortLink: URL
     public let previewLink: URL
+    public let warning: [Warning]?
 
+    public struct Warning: FirebaseModel {
+        public let code: String
+        public let message: String
+
+        enum CodingKeys : String, CodingKey {
+            case code = "warningCode"
+            case message = "warningMessage"
+        }
+    }
 }
