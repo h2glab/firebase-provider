c5c10732ee9635174d5574235913ffd1cb2b1a9c
Martial Maillot <martial@h2g.io>
2019-01-09 10:30:43 +0100

    Minor update on string random method, use 0..<X in place of 0..X-1

--- Tests/FirebaseTests/Helpers/String+Random.swift
+++ Tests/FirebaseTests/Helpers/String+Random.swift
@@ -3,6 +3,6 @@ import Foundation
 extension String {
     static func random(length: Int) -> String {
         let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        return String((0...length-1).map{ _ in characters.randomElement()! })
+        return String((0..<length).map{ _ in characters.randomElement()! })
     }
 }
